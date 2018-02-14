8aa9bd74fe379187b4bca729fcc791e0ab24a7f5
Pegas13fps <Tymoschenkob@gmail.com>
2018-02-14 19:33:09 +0200

    BeetRoot API Fianl

--- main.js
+++ main.js
@@ -1,38 +1,48 @@
+
+
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
+
     let city = document.querySelector('.city');
     let img = document.querySelector('.weather-icon');
     let deg = document.querySelector('.temp');
     let card = document.querySelector('.card');
-    let preloader = document.querySelector('preloader');
-    let btn = document.querySelector('btn');
-    
+    let preloader = document.querySelector('.preloader');
+    let btn = document.querySelector('.upd');
+
     const WEATHER_PATH = 'http://api.openweathermap.org/data/2.5/weather';
-    const API_KEY = 'fc21ad824e2f1b103d15f80845f27f73';
+    const API_KEY = '22603d94d2580dd4da189c31ff13a4a6';
 
     const getData = () => {
-        if (navigator.geolocation) {
+        if(navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(position => {
                 let latitude = position.coords.latitude;
                 let longitude = position.coords.longitude;
-                fetch(`${WEATHER_PATH}?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`) 
+                fetch(`${WEATHER_PATH}?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
                     .then(r => r.json())
                     .then(data => {
+                        
                         city.textContent = data.name;
-                        img.setAttribute('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+                        img.setAttribute('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`)
                         deg.textContent = (data.main.temp - 272.15).toFixed(1);
                         card.style.display = 'block';
                         preloader.style.display = 'none';
                     })
+            
+            }, () => {
+                preloader.textContent = 'Нет доступа!';
             })
         } else {
-            preloader.textContent = 'Пользователь не разрешает нам видеть позицию...';
-        } 
+            preloader.textContent = 'У пользователя старый браузер!!!';
+        }
     }
     getData();
+
     btn.addEventListener('click', () => {
         card.style.display = 'none';
-        preloader.style.display = 'none';
-    })
+        preloader.style.display = 'block';
+        getData();
+    });
+
 }
